de942f02972ffcd193bf3e25e1afda5093d9b33a
shiyu22 <cshiyu22@gmail.com>
2020-05-08 18:52:30 +0800

    update

--- webserver/src/app.py
+++ webserver/src/app.py
@@ -59,7 +59,7 @@ def do_insert_api():
 
 @app.route('/data/<image_name>')
 def image_path(image_name):
-    file_name = UPLOAD_FOLDER + '/' + image_name
+    file_name = UPLOAD_PATH + '/' + image_name
     print(file_name)
     if path.exists(file_name):
         return send_file(file_name)
