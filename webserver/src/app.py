8a0ef17440445b299bf48f2a0bd53a2602f7c7eb
shiyu22 <cshiyu22@gmail.com>
2020-05-08 18:40:58 +0800

    update

--- webserver/src/app.py
+++ webserver/src/app.py
@@ -77,14 +77,10 @@ def do_search_api():
         voc_path = os.path.join(app.config['UPLOAD_FOLDER'], ids + '.wav')
         file_voc.save(voc_path)
 
-        try:
-            res = do_search(img_path, voc_path)
-            # res[1] = request.url_root + "data/" + str(res[1]) + '.png'
-            print("------a",res)
-        except:
-            return "There has no results, please make sure there is only one face in the video."
+        res = do_search(img_path, voc_path)
+        res[1] = request.url_root + "data/" + str(res[1]) + '.png'
+        print("------a",res)
 
-        
         return "{}".format(res), 200
     return "not found", 400
 
