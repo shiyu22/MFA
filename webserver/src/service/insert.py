472d2b3b682cbdd135194fc0495b9a3e3203db3f
shiyu22 <cshiyu22@gmail.com>
2020-05-08 16:47:26 +0800

    update

--- webserver/src/service/insert.py
+++ webserver/src/service/insert.py
@@ -58,6 +58,7 @@ def insert_data_to_milvus(ids, img, voc):
     try:
         insert_vectors(index_client, IMG_TABLE, [vectors_img], [ids])
         status = insert_vectors(index_client, VOC_TABLE, [vectors_voc], [ids])
+        print(status)
         return status
     except Exception as e:
         logging.error(e)
