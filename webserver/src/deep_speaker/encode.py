b7414784b64a3150864ec7ef0bd1f3883e50e955
shiyu22 <cshiyu22@gmail.com>
2020-05-08 16:07:15 +0800

    update

--- webserver/src/deep_speaker/encode.py
+++ webserver/src/deep_speaker/encode.py
@@ -7,14 +7,14 @@ from deep_speaker.conv_models import DeepSpeakerModel
 from deep_speaker.test import batch_cosine_similarity
 
 
-def voc_to_vec():
+def voc_to_vec(voc):
     np.random.seed(123)
     random.seed(123)
 
     model = DeepSpeakerModel()
     model.m.load_weights('checkpoints/ResCNN_triplet_training_checkpoint_265.h5', by_name=True)
 
-    mfcc = sample_from_mfcc(read_mfcc(file, SAMPLE_RATE), NUM_FRAMES)
+    mfcc = sample_from_mfcc(read_mfcc(voc, SAMPLE_RATE), NUM_FRAMES)
     predict = model.m.predict(np.expand_dims(mfcc, axis=0))
     vec = list(map(float,predict.tolist()[0]))
 
